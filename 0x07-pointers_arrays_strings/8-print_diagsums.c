505812c2d70dbaec7e8651f0e06b81cd80a39195
hajarmos <hajarbouzaid@gmail.com>
2023-04-03 18:59:16 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,17 +12,17 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, res1 = 0, res2 = 0, p;
+	int i, j, res1 = 0, res2 = 0, p;
 
 	for (i = 0; i < size; i++)
 	{
 		p = (i * size) + i;
 		res1 += *(a + p);
 	}
-	for (i = 0; i < size; i++)
+	for (j = 0; j < size; j++)
 	{
-		p = (i * size) + (size - 1 - i);
+		p = (j * size) + (size - 1 - j);
 		res2 += *(a + p);
 	}
-	printf("%d, %d", res1, res2);
+	printf("%d, %d\n", res1, res2);
 }
