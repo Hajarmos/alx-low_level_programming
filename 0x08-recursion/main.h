0fd41d898f142d0f3a78dfe86169703d644528a8
hajarmos <hajarbouzaid@gmail.com>
2023-04-05 16:12:31 -0700

    task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,3 @@
-#ifndef main_h
-#ifdef main_h
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -10,5 +8,3 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
-
-#endif
