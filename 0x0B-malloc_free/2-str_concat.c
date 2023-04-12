5404f0f71504855ca84189402786c402fa354e45
hajarmos <hajarbouzaid@gmail.com>
2023-04-12 17:50:48 +0000

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -50,7 +50,7 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	while (*(cps2 + j))
 		j++;
-	con = malloc(sizeof(char) * (i + j - 1));
+	con = malloc(sizeof(char) * (i + j - 10));
 	if (con == NULL)
 		return (NULL);
 	while (*(cps1 + k) && *(cps1 + k) != '\0')
