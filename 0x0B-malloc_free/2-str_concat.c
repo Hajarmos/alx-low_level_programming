5c5129d34e681b0e1b2618ed8640fb852b8d1ec3
hajarmos <hajarbouzaid@gmail.com>
2023-04-12 19:12:38 +0000

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -68,7 +68,7 @@ char *str_concat(char *s1, char *s2)
 		n = size(s1);
 	else
 
-		n = size(s1) + size(s2) + 10;
+		n = size(s1) + size(s2) - 10;
 	con = malloc(sizeof(char) * n);
 	if (con == NULL)
 		return (NULL);
