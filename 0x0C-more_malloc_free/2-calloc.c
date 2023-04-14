d66c612ec4e6d092c061752a94ff5b91aeb2923b
hajarmos <hajarbouzaid@gmail.com>
2023-04-14 17:50:12 +0000

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	all = malloc(size * nmemb);
 	if (all == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < size * nmemb; i++)
 		*(all + i) = 0;
 	return ((void *)all);
 }
