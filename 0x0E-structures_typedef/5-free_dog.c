9f83a7d98849e30cc702c2f99e4b9889d9c02fc9
hajarmos <hajarbouzaid@gmail.com>
2023-04-18 01:13:03 +0000

    Frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dogs
+ * @d: variable of dog_t
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+	}
+}
