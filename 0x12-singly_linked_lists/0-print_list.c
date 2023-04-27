be6dee2ed697e2941e6ef7dd25f4f6613add2548
hajarmos <hajarbouzaid@gmail.com>
2023-04-27 23:16:30 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 
 	while (temp)
 	{
-		if (!temp->str)
+		if (temp->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", temp->len, temp->str);
