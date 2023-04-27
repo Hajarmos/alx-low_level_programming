d3d88cc0dac5277b89a907a5b52361cac45071fd
hajarmos <hajarbouzaid@gmail.com>
2023-04-27 23:21:52 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,14 +11,12 @@ size_t print_list(const list_t *h)
 	unsigned int n = 0;
 	const list_t *temp = h;
 
-	while (temp)
+	for (; temp; temp = temp->next, n++)
 	{
 		if (temp->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", temp->len, temp->str);
-		n++;
-		temp = temp->next;
 	}
 	return (n);
 }
