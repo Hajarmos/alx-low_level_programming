16f99814302d8ca0d4352ae77adacd62d0a60449
hajarmos <hajarbouzaid@gmail.com>
2023-04-30 20:45:12 +0000

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -35,10 +35,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			temp->next = tempp->next;
 			free(tempp);
+			return (1);
 		}
 		else
 			return (-1);
 	}
-	return (1);
 }
 
