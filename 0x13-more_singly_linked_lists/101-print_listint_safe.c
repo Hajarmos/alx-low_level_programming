88fb96e247a368fca427a112c403b382db3e943b
hajarmos <hajarbouzaid@gmail.com>
2023-05-02 17:12:42 +0000

    Print safe version

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: first list's element
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *temp, *check;
+	unsigned int i = 0, j;
+
+	temp = head;
+	while (temp)
+	{
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		i++;
+		temp = temp->next;
+		check = head;
+		j = 0;
+		while (j < i)
+		{
+			if (temp == check)
+			{
+				printf("->[%p] %d\n", (void *)temp, temp->n);
+				return (i);
+			}
+			check = check->next;
+			j++;
+		}
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: first element in list
+ * Return: the size of the list that was free’d
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp, *check, *head;
+	unsigned int i = 0, j;
+
+	head = *h;
+	temp = *h;
+	while (temp)
+	{
+		i++;
+		*h = temp->next;
+		free(temp);
+		temp = *h;
+		check = head;
+		j = 0;
+		while (j < i)
+		{
+			if (temp == check)
+			{
+				*h = NULL;
+				return (i);
+			}
+			check = check->next;
+			j++;
+		}
+	}
+	*h = NULL;
+        return (i);
+}
