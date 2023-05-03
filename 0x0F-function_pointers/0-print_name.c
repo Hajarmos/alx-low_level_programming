b2f558cc2073e895ca192570075aaf5f5802011b
hajarmos <hajarbouzaid@gmail.com>
2023-05-03 10:01:49 +0000

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+
+/**
+ * print_name - prints a name
+ * @name: name*
+ * @f: pointer to function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
