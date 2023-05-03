735429c2e2399d8deedbe56b7d776de0b41c6a23
hajarmos <hajarbouzaid@gmail.com>
2023-05-03 01:21:14 +0000

    fix Segmentation fault problem

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,10 +11,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *fast = head, *slow = head;
 
-	if (!head)
-		return (NULL);
-
-	while (fast->next && slow && fast)
+	while (fast->next->next && slow && fast)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
