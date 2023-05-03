68cee90aeba9bfb333f41f3899568dcd29b182c5
hajarmos <hajarbouzaid@gmail.com>
2023-05-03 01:25:55 +0000

    update

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,7 +11,9 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *fast = head, *slow = head;
 
-	while (fast->next->next && slow && fast)
+	if (head == head->next)
+		return (head);
+	while (fast->next->next && slow->next && fast)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
