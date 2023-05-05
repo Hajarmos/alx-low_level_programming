a4d324f3bca9e8ff95df1c0d97538e0a243d531b
hajarmos <hajarbouzaid@gmail.com>
2023-05-05 00:15:18 +0000

    Print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: input number
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int cp;
+	int i = 0;
+
+	if (!n)
+	{
+		_putchar('0');
+		return;
+	}
+	cp = n;
+	while (cp)
+	{
+		cp >>= 1;
+		i++;
+	}
+	while (i)
+	{
+		if (n >> i)
+			_putchar('1');
+		else
+			_putchar('0');
+		i--;
+	}
+}
+
