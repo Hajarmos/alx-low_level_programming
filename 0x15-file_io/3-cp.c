ebec0697dae1f936bec06f5f20eee7bb69717671
hajarmos <hajarbouzaid@gmail.com>
2023-05-09 13:44:25 +0000

    cp command

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]),
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
@@ -40,13 +40,13 @@ int main(int argc, char **argv)
 		rd = read(file_from, &buffer, 1024);
 	}
 	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n",argv[1]),
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]),
 			exit(100);
 	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n",argv[2]),
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]),
 			exit(100);
 	if (rd == -1)
-		 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-			 exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
 	return (0);
 }
